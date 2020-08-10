1915d9cd46ab1e39b63447cd73edad87780050f8
nithishdnr <nithishdnr3@gmail.com>
2020-08-10 21:33:24 +0530

    Updates

---
+++ precache-manifest.ad86b985434b8f4189c28a5956404803.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49a031642e64e4cc10667cabd23829bd",
+    "url": "/myblog/index.html"
+  },
+  {
+    "revision": "b02dc8f3606ed46f1ef7",
+    "url": "/myblog/static/css/main.806cc6e4.chunk.css"
+  },
+  {
+    "revision": "04d755324b2888dff309",
+    "url": "/myblog/static/js/2.e38da9ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myblog/static/js/2.e38da9ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b02dc8f3606ed46f1ef7",
+    "url": "/myblog/static/js/main.95cb8e74.chunk.js"
+  },
+  {
+    "revision": "614a9843fb658e1eba5b",
+    "url": "/myblog/static/js/runtime-main.d58387fb.js"
+  },
+  {
+    "revision": "0fca0667cd92f46c3b3b47093e340018",
+    "url": "/myblog/static/media/circle-cropped-1.0fca0667.png"
+  }
+]);
